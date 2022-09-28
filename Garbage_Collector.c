5460af84cbdc8688a4d742a0fa6956219211d4c5
FabricioLourran <fabriciolourran0007@gmail.com>
2022-09-28 01:24:31 -0300

    coletor de lixo por lista encadeada

--- Garbage_Collector.c
+++ Garbage_Collector.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 
+
+//Struct com espaço pro endereço de memória e o contador 
 typedef struct Memory{
     int *pointer;
     int counter;
@@ -16,37 +18,41 @@ int *malloc2(int size){
     vetor[count_memory].counter = 1;
     vetor[count_memory].pointer = address;
     count_memory++;
-    printf("%d\n",address);
     return address;
 }
 
+//Os 2 parâmetros são os vetores, ou um valor nulo no segundo parâmetro
 void atrib2(int *address1, int *address2){
-	for(int i = 0; i < 50;i++){
+
+	for(int i = 0; i < count_memory;i++){
         
-        if(vetor[i].pointer==address1 && vetor[i].counter==1 && address2==NULL ){
-            vetor[i].counter=0;
+        if(vetor[i].pointer == address1 && vetor[i].counter == 1 && address2 == NULL ){
+            vetor[i].counter = 0;
         }
-        else if(vetor[i].pointer==address1 && vetor[i].counter!=1 && address2==NULL ){
+
+        else if(vetor[i].pointer == address1 && vetor[i].counter != 1 && address2 == NULL ){
             vetor[i].counter--;
         }
 
-        else if(vetor[i].pointer==address1){
+        else if(vetor[i].pointer == address1){
             vetor[i].counter--;
-            if(vetor[i].counter<=0){
+
+            if(vetor[i].counter <= 0){
                 free(vetor[i].pointer);
             }
         }
 
-        if(vetor[i].pointer==address2){
+        if(vetor[i].pointer == address2){
             vetor[i].counter++;
         }
+        printf("\n%d\n",i);
     }
 }
 
 void dump(){
 
-    if(vetor==NULL){
-        printf("Nenhuma memória alocada");
+    if(count_memory == 0){
+        printf("Nenhuma memória alocada\n");
     }
 
     for(int position = 0; position < count_memory; position++){
@@ -58,10 +64,18 @@ void dump(){
 
 
 int main(){
+
     dump();
+
     int *v = malloc2(sizeof(int));
     int *w = malloc2(sizeof(int));
+    int *q = malloc2(sizeof(int));
+    int *s = malloc2(sizeof(int));
+    
     atrib2(v,w);
+    atrib2(v,q);
+    
+
     dump();
     
 
---
+++ Garbage_ColletorLista.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct memory{
+    int counter;
+    int *address;
+    struct memory *prox;
+}memory;
+
+memory *list = NULL;
+
+
+void *malloc2(int size){
+
+    
+    memory *aux = malloc(sizeof(memory)); //Aloca a memória do tamanho do struct
+    memory *aux2 = malloc(sizeof(memory)); //Auxiliar para inserir no fim
+    aux->address = (int*)malloc(size); //O endereço do struct recebe o malloc solicitado na main
+    aux->counter = 1; //A cada malloc feito, aquele endereço terá 1 no contador de referência
+    aux->prox = NULL;
+
+    if(list != NULL){
+     aux->prox = list;
+    }
+
+    list = aux;
+    //printf("Ponteiro:%d\n", aux->address);
+    return list->address; //Retorna o endereço do malloc
+}
+
+void dump(){
+    memory *aux = list;
+
+    if(list == NULL){ //Se a lista for vazia a memória não foi alocada
+        printf("Nenhuma memória alocada\n");
+    }
+
+    while(aux!= NULL){ //Enquanto a lista não for vazia, printa  a memória alocada
+        printf("{Endereço: %p | contador: %d}\n",aux->address,aux->counter);
+        aux = aux->prox;
+    }
+}
+
+memory* elementRemove(memory *list, int *address_){
+    memory *aux;
+
+    if(list==NULL){
+        return list;
+    }
+
+    else if(list->address != address_){ //Se o endereço que for dado free for diferente 
+                                       //do que está no struct, entra em recursão
+        list->prox = elementRemove(list->prox,address_);
+        return list;
+    }
+
+    else{
+        aux = list;
+        list = list->prox;
+        free(aux);
+        list= elementRemove(list,address_);
+        return list;
+    }
+}
+
+void atrib2(int *address1, int *address2){
+    memory *aux = list;
+
+    while(aux != NULL){
+
+        if(aux->address == address1){ //Vai decrementar o contador do primeiro ponteiro passado
+            aux->counter -= 1;
+        
+            if(aux->counter == 0){ //Se o contador for igual a 0, será dado free na alocação
+                list = elementRemove(list,address1);
+            }
+            
+        }
+
+        if(aux->address == address2){ //No segundo ponteiro será incrementado 1 no contador
+            aux->counter += 1;
+        }
+        aux = aux->prox; //Passa para o próximo nó
+    }
+
+}
+
+int main(){
+    dump();
+    printf("\n-------------------------------------\n");
+    int *v = malloc2(sizeof(int));
+    int *s = malloc2(sizeof(int));
+    int *w = malloc2(sizeof(int));
+    dump();
+    atrib2(v,s);
+    atrib2(w,s);
+    printf("\n-------------------------------------\n");
+    
+    dump();
+    
+
+
+    return 0;
+}
+
